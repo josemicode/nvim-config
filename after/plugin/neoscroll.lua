dbb2098ef930024f81467002b3ad80be2acc6d69
josemicode <jmiguelojedagutierrez@gmail.com>
2025-05-24 12:19:34 +0200

    scroll + move line

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -21,7 +21,7 @@ require("neoscroll").setup({
 	mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
 		'<C-u>', '<C-d>',
 		'<C-b>', '<C-f>',
-		'<C-y>', '<C-e>',
+		'<C-y>',
 		'zt', 'zz', 'zb',
 		'gg',
 	},
--- lua/josemicode/remap.lua
+++ lua/josemicode/remap.lua
@@ -1,2 +1,6 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.g.mapleader = " " -- <leader>
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- netrw
+
+-- Move line
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { silent = true })
